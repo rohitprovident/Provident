0edf0fb34f5df24dc519f8af496a6a53b15e5ec1
rohitprovident <59652465+rohitprovident@users.noreply.github.com>
2020-01-09 08:58:48 +0000

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,1 +1,26 @@
 
+<?php
+echo "Hello world!";
+    
+?>
+<html>
+<body>
+
+<table border="2">
+    <tr>
+        <th>name</th>
+        <th>number</th>
+
+    </tr>
+    <?php
+    $conn = mysqli_connect("localhost","providentcrm","providentcrm","default");
+    $q2 = "select * from abc ";
+    $run = mysqli_query($conn, $q2);
+    $row = mysqli_fetch_array($run);
+    ?>
+    <tr>
+        <td><?= $row['name']; ?></td>
+        <td><?= $row['number']; ?></td>
+    </tr>
+</table>
+</body>
